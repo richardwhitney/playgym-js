db86093cdbdf6172815f474fab3680a4f8664a90
Glitch (play-gym-basline) <none>
2018-07-25 22:17:07 +0000

    🚵🌑 Checkpoint ./controllers/dashboard.js:1388891/18 ./models/assessment-store.json:1388891/185

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -6,7 +6,7 @@ const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
-      title: 'Template 1 Dashboard',
+      title: 'Play Gym Dashboard',
     };
     response.render('dashboard', viewData);
   },
