5df3631787a5bba511ca8d1ce76268067f921b92
Glitch (play-gym-basline) <none>
2018-07-25 23:10:45 +0000

    🐧🕷 Checkpoint ./controllers/dashboard.js:1388891/103

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const assessmentCollection = require('../models/assessment.store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
       title: 'Play Gym Dashboard',
+      assessments: assessment
     };
     response.render('dashboard', viewData);
   },
