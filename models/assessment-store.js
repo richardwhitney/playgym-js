0743af1ad513d15d5581c43b61a59fccca27f518
Glitch (play-gym-basline) <none>
2018-08-01 16:42:51 +0000

    📹🎫 Checkpoint ./models/assessment-store.js:1388891/1199 ./models/json-store.js:1388891/966

--- models/assessment-store.js
+++ models/assessment-store.js
@@ -1,26 +1,37 @@
 'use strict';
 
 const _ = require('lodash');
+const JsonStore = require('./json-store');
 
 const assessmentStore = {
   
-  assessmentCollection: require('./assessment-store.json').assessmentCollection,
+  store: new JsonStore('./models/assessment-store.json', { assessmentCollection: [] }),
+  collection: 'assessmentCollection',
   
   getAllAssessments() {
-    return this.assessmentCollection;
+    return this.store.findAll(this.collection);
   },
   
   getAssessment(id) {
-    return _.find(this.assessmentCollection, {id:id});
+    return this.store.findOneBy(this.collection, { id: id});
   },
   
   addAssessment(assessment) {
-    this.assessmentCollection.push(assessment);
+    this.store.add(this.collection, assessment);
+    this.store.save();
+  },
+  
+  removeAssessment(id) {
+    const assessment = this.getAssessment(id);
+    this.store.remove(this.collection, assessment);
+    this.store.save();
+  },
+  
+  removeAllAssessments() {
+    this.store.removeAll(this.collection);
+    this.store.save();
   },
   
 };
-
-
-
 
 module.exports = assessmentStore;
